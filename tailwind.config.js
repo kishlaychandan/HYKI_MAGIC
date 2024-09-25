3cb4fb69cec861c77b36d44779a791712f3a2d31
shirajahmed <shirajad690@gmail.com>
2024-09-26 01:45:35 +0530

    apply darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
 
-        // Or if using `src` directory:
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        fontFamily: {
-            oswald: "Oswald, sans-serif",
-            urbanist: "Urbanist, sans-serif",
-        },
-        extend: {},
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      oswald: "Oswald, sans-serif",
+      urbanist: "Urbanist, sans-serif",
     },
-    plugins: [],
+    extend: {},
+  },
+  plugins: [],
 };
