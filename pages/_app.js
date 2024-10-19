8ab19b4bf9a1220d5cd2562ddf55d268435f123c
“kishlaychandan00” <kishlaychandan00@email.com>
2024-10-19 20:28:10 +0530

    social media integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import Head from "next/head";
-
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "next-themes";
 import ChatBots from "@/components/ChatBot/ChatBots";
-
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -25,12 +26,28 @@ export default function App({ Component, pageProps }) {
           rel="stylesheet"
         />
       </Head>
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Header />
-          <Component {...pageProps} />
-          <ChatBots />
-          <Footer />
-        </ThemeProvider>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Header />
+        <Component {...pageProps} />
+        <ChatBots />
+        <div className=" flex items-center justify-center gap-6  fixed mb-3 bottom-0 left-[48%] p-2">
+          <Link
+          // insta account: hyki.kmk
+            href="https://www.instagram.com/hyki.kmk"
+            className="w-10 h-10 bg-blue-700 text-white  rounded-full  flex items-center justify-center  cursor-pointer"
+          >
+            <FaInstagram size={24} />
+          </Link>
+          <Link
+            href="https://wa.me/7026202925"
+            className="w-10 h-10 bg-green-600 text-white rounded-full flex items-center justify-center   cursor-pointer"
+          >
+            <FaWhatsapp size={24} />
+          </Link>
+        </div>
+        <Footer />
+      </ThemeProvider>
+      
     </>
   );
 }
