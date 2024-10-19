56010e798784fc87f5711022ff1d1f2721194e09
“kishlaychandan00” <kishlaychandan00@email.com>
2024-10-19 21:08:39 +0530

    updating banner image

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,70 +1,49 @@
 import React from "react";
-
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from "react-responsive-carousel";
 import Link from "next/link";
-
 import { BiArrowBack } from "react-icons/bi";
 
 const HeroBanner = () => {
     return (
-        <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto">
+        <div className="relative w-full max-w-[1600px] mx-auto">
             <Carousel
                 autoPlay={true}
                 infiniteLoop={true}
                 showThumbs={false}
                 showIndicators={false}
                 showStatus={false}
-                renderArrowPrev={(clickHandler, hasPrev) => (
+                renderArrowPrev={(clickHandler) => (
                     <div
                         onClick={clickHandler}
-                        className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+                        className="absolute right-12 md:right-24 bottom-4 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
                     >
-                        <BiArrowBack className="text-sm md:text-lg" />
+                        <BiArrowBack className="text-white text-sm md:text-lg" />
                     </div>
                 )}
-                renderArrowNext={(clickHandler, hasNext) => (
+                renderArrowNext={(clickHandler) => (
                     <div
                         onClick={clickHandler}
-                        className="absolute right-0 bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+                        className="absolute right-2 md:right-4 bottom-4 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
                     >
-                        <BiArrowBack className="rotate-180 text-sm md:text-lg" />
+                        <BiArrowBack className="rotate-180 text-white text-sm md:text-lg" />
                     </div>
                 )}
             >
-            
-                <div>
-                    <img
-                        src="/kamakhyaBanner1.jpg"
-                        className="aspect-[16/10] md:aspect-auto object-cover"
-                    />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-                        {/* Shop now */}
-                        <Link href="https://hyki.mini.store" >Shop now</Link>
+                {["/s1.jpg", "/s2.jpg", "/kamakhyaBanner1.jpg"].map((src, index) => (
+                    <div key={index} className="relative">
+                        <img
+                            src={src}
+                            alt={`Banner ${index + 1}`}
+                            className="w-full h-full object-contain"
+                        />
+                        <div className="absolute bottom-6 left-4 md:bottom-10 md:left-10 bg-white px-4 py-2 md:px-8 md:py-4 font-oswald text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+                            <Link href="https://hyki.mini.store" target="_blank" rel="noopener noreferrer">
+                                Shop now
+                            </Link>
+                        </div>
                     </div>
-                </div>
-
-                <div>
-                    <img
-                        src="/kamakhyaBanner2.jpg"
-                        className="aspect-[16/10] md:aspect-auto object-cover"
-                    />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-                        <Link href="https://hyki.mini.store" >Shop now</Link>
-                        
-                    </div>
-                </div>
-
-                <div>
-                    <img
-                        src="/kamakhyaBanner.jpg"
-                        className="aspect-[16/10] md:aspect-auto object-cover"
-                    />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-                        {/* Shop now */}
-                        <Link href="https://hyki.mini.store" >Shop now</Link>
-                    </div>
-                </div>
+                ))}
             </Carousel>
         </div>
     );
